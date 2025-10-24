b729fb895f689083112d7409693cc8423fbfbdd9
nonna-tech <receitasnonnaangela1@gmail.com>
2025-10-24 15:03:42 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,25 @@
-const CACHE = 'mr-v1';
+const CACHE = 'mr-v2';
+const ASSETS = [
+  'index.html',
+  'css/style.css',
+  'js/app.js',
+  'manifest.json',
+  'img/icon-192.png',
+  'img/icon-512.png'
+];
+
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll([
-    './','/','index.html','css/style.css','js/app.js','manifest.json','img/icon-192.png','img/icon-512.png'
-  ])));
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
 });
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+});
+
 self.addEventListener('fetch', e => {
   e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
